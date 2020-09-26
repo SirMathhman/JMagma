8e6632df6c6f43f445ae7ca621bdb5056b51d00f
SirMathhman <sirmathhman@gmail.com>
2020-09-26 18:46:54 -0400

    Add loading in build file

--- src/main/java/com/meti/Main.java
+++ src/main/java/com/meti/Main.java
@@ -1,7 +1,26 @@
 package com.meti;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class Main {
+    private static final Logger logger = Logger.getAnonymousLogger();
+
+    public static void main(String[] args) {
+        Path build = Paths.get(".", ".build");
+        try {
+            if (!Files.exists(build)) {
+                String message = "Configuration file did not exist and is being created at: %s";
+                logger.log(Level.SEVERE, String.format(message, build.toAbsolutePath()));
+                Files.createFile(build);
+            }
+        } catch (IOException e) {
+            String format = "Failed to create configuration file at: %s";
+            logger.log(Level.SEVERE, String.format(format, build.toAbsolutePath()), e);
+        }
     }
 }
