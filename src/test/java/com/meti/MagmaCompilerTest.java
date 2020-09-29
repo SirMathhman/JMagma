6c701208e5a9efa3879f4c11ef0004bd7687c15a
SirMathhman <sirmathhman@gmail.com>
2020-09-28 23:48:41 -0400

    Add tests for declarations

--- src/test/java/com/meti/MagmaCompilerTest.java
+++ src/test/java/com/meti/MagmaCompilerTest.java
@@ -6,8 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MagmaCompilerTest {
+    private static final Compiler Compiler = new MagmaCompiler();
 
-    public static final Compiler Compiler = new MagmaCompiler();
+    @Test
+    void declareConstant(){
+        assertEquals("int x=10;", Compiler.compileToString("const x : I16 = 10"));
+    }
+
+    @Test
+    void declareVariable(){
+        assertEquals("int x=10;", Compiler.compileToString("let x : I16 = 10"));
+    }
 
     @Test
     void testFloat(){
