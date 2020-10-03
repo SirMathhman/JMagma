f6a6a42aa53b03c4ad28a3078e9046ffd50ed175
SirMathhman <sirmathhman@gmail.com>
2020-10-03 18:21:18 -0400

    Remove bad code

--- src/main/java/com/meti/api/script/Package.java
+++
@@ -1,5 +0,0 @@
-package com.meti.api.script;
-
-public interface Package {
-
-}
--- src/main/java/com/meti/api/script/ScriptLoader.java
+++
@@ -1,5 +0,0 @@
-package com.meti.api.script;
-
-public interface ScriptLoader {
-    String load(Package package0);
-}
