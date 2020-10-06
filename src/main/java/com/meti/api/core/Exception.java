fbca9913a27efaacc913056e931a7cc0873e4198
SirMathhman <sirmathhman@gmail.com>
2020-10-06 15:46:49 -0400

    Add Option

--- src/main/java/com/meti/api/core/Exception.java
+++
@@ -1,7 +0,0 @@
-package com.meti.api.core;
-
-public class Exception extends RuntimeException {
-    public Exception(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/meti/reference/Option.java
@@ -0,0 +1,35 @@
+package com.meti.reference;
+
+import java.util.function.Function;
+
+public interface Option<T> {
+    static <T> Some<T> Some(T value) {
+        return new Some<>(value);
+    }
+
+    static <T> None<T> None() {
+        return new None<>();
+    }
+
+    <R> R transformOrElse(Function<T, R> function, R other);
+
+    class Some<T> implements Option<T> {
+        private final T value;
+
+        public Some(T value) {
+            this.value = value;
+        }
+
+        @Override
+        public <R> R transformOrElse(Function<T, R> function, R other) {
+            return function.apply(value);
+        }
+    }
+
+    class None<T> implements Option<T> {
+        @Override
+        public <R> R transformOrElse(Function<T, R> function, R other) {
+            return other;
+        }
+    }
+}
