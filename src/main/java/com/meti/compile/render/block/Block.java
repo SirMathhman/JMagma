762a87f2e36f9f706db4659ac4590938e5bd0182
SirMathhman <sirmathhman@gmail.com>
2020-10-08 11:39:21 -0400

    Add Blocks

---
+++ src/main/java/com/meti/compile/render/block/Block.java
@@ -0,0 +1,39 @@
+package com.meti.compile.render.block;
+
+import com.meti.compile.render.node.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Block implements EmptyNode, UnfieldedNode, UnidentifiedNode, UnvaluedNode {
+    private final List<Node> children;
+
+    public Block(List<Node> children) {
+        this.children = Collections.unmodifiableList(children);
+    }
+
+    @Override
+    public Node mapByChildren(Function<Node, Node> mapper) {
+        return new Block(mapChildren(mapper));
+    }
+
+    private List<Node> mapChildren(Function<Node, Node> mapper) {
+        return children.stream()
+                .map(mapper)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean is(Group group) {
+        return group == Group.Block;
+    }
+
+    @Override
+    public String render() {
+        return children.stream()
+                .map(Node::render)
+                .collect(Collectors.joining("", "{", "}"));
+    }
+}
