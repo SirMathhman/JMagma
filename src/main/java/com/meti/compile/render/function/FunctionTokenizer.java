422e712ebe4529d7842cfef82fe0960c545f43d0
SirMathhman <sirmathhman@gmail.com>
2020-10-08 12:29:32 -0400

    Prepare for adding implicit return types

--- src/main/java/com/meti/compile/render/function/FunctionTokenizer.java
+++ src/main/java/com/meti/compile/render/function/FunctionTokenizer.java
@@ -5,9 +5,12 @@ import com.meti.compile.render.field.Field;
 import com.meti.compile.render.field.FieldTokenizer;
 import com.meti.compile.render.node.ContentNode;
 import com.meti.compile.render.node.Node;
+import com.meti.compile.render.primitive.ImplicitType;
 import com.meti.compile.render.type.ContentType;
+import com.meti.compile.render.type.Type;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -19,24 +22,57 @@ public class FunctionTokenizer extends AbstractNodeTokenizer {
     @Override
     public Optional<Node> evaluate() {
         if (content.startsWith("def")) {
-            var paramStart = content.indexOf('(');
-            var paramEnd = content.indexOf(')');
-            var parameters = Arrays.stream(content.substring(paramStart + 1, paramEnd)
-                    .trim().split(","))
-                    .filter(s -> !s.isBlank())
-                    .map(String::trim)
-                    .map(this::tokenizeParameter)
-                    .collect(Collectors.toList());
-            var name = content.substring(4, paramStart).trim();
-            var returnIndex = content.indexOf(':', paramEnd + 1);
-            var valueIndex = content.indexOf("=>");
-            var trim = content.substring(returnIndex + 1, valueIndex).trim();
-            var returnType = new ContentType(trim);
-            var valueString = content.substring(valueIndex + 2).trim();
+            var parameters = tokenizeParameters();
+            var name = content.substring(4, content.indexOf('(')).trim();
+            var returnIndex = content.indexOf(':', content.indexOf(')') + 1);
+            var returnType = tokenizeReturnType(returnIndex);
+            var valueString = content.substring(content.indexOf("=>") + 2).trim();
             var value = new ContentNode(valueString);
             return Optional.of(new Function(name, parameters, returnType, value));
         }
         return Optional.empty();
+    }
+
+    private Type tokenizeReturnType(int returnIndex) {
+        Type returnType;
+        if (returnIndex == -1) {
+            returnType = ImplicitType.ImplicitType;
+        } else {
+            String trim;
+            trim = content.substring(returnIndex + 1, content.indexOf("=>")).trim();
+            returnType = new ContentType(trim);
+        }
+        return returnType;
+    }
+
+    private List<Field> tokenizeParameters() {
+        return extractParameters()
+                .stream()
+                .filter(s -> !s.isBlank())
+                .map(String::trim)
+                .map(this::tokenizeParameter)
+                .collect(Collectors.toList());
+    }
+
+    private List<String> extractParameters() {
+        if (content.contains("(") && content.contains(")")) {
+            var paramStart = content.indexOf('(');
+            var paramEnd = content.indexOf(')');
+            var substring = content.substring(paramStart + 1, paramEnd);
+            var trim = substring.trim();
+            var split = trim.split(",");
+            return List.of(split);
+        } else if(!content.contains("(") && !content.contains(")")) {
+            return Collections.emptyList();
+        } else if(!content.contains("(")) {
+            var format = "'%s' does not have a parameter start indicator.";
+            var message = format.formatted(content);
+            throw new IllegalStateException(message);
+        } else {
+            var format = "'%s' does not have a parameter end indicator.";
+            var message = format.formatted(content);
+            throw new IllegalStateException(message);
+        }
     }
 
     private Field tokenizeParameter(String s) {
--- src/test/java/com/meti/compile/render/function/FunctionTest.java
+++ src/test/java/com/meti/compile/render/function/FunctionTest.java
@@ -3,24 +3,39 @@ package com.meti.compile.render.function;
 import com.meti.compile.render.FeatureTest;
 import org.junit.jupiter.api.Test;
 
-public class FunctionTest extends FeatureTest  {
+public class FunctionTest extends FeatureTest {
     @Test
-    void simple(){
+    void implicit(){
+        assertCompile("int supplier(){return 10;}", "def supplier() => {return 10;}");
+    }
+
+    @Test
+    void withoutBlock(){
+        assertCompile("int supplier(){return 10;}", "def supplier() => return 10");
+    }
+
+    @Test
+    void withoutReturn(){
+        assertCompile("int supplier(){return 10;}", "def supplier() => 10");
+    }
+
+    @Test
+    void simple() {
         assertCompile("void procedure(){}", "def procedure() : Void => {}");
     }
 
     @Test
-    void returnType(){
+    void returnType() {
         assertCompile("int supplier(){}", "def supplier() : I16 => {}");
     }
 
     @Test
-    void singleParameter(){
+    void singleParameter() {
         assertCompile("void consumer(int value){}", "def consumer(const value : I16) : Void => {}");
     }
 
     @Test
-    void multipleParameters(){
+    void multipleParameters() {
         assertCompile("void consumer(int value0,int value1){}", """
                 def consumer(const value0 : I16, const value1 : I16) : Void => {
                 }
