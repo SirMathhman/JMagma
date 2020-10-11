48c1a6381327ec854bd02de57e0fa59f1532b5fa
SirMathhman <sirmathhman@gmail.com>
2020-10-10 21:37:10 -0400

    Introduce new cache methods

--- src/main/java/com/meti/compile/render/process/InlineState.java
+++ src/main/java/com/meti/compile/render/process/InlineState.java
@@ -2,13 +2,49 @@ package com.meti.compile.render.process;
 
 import com.meti.compile.render.node.Node;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class InlineState implements State {
     private final Node value;
     private final Stack scope;
+    private final List<Node> functions;
+    private final List<Node> structures;
 
     public InlineState(Node value, Stack scope) {
+        this(value, scope, new ArrayList<>(), new ArrayList<>());
+    }
+
+    public InlineState(Node value, Stack scope, List<Node> functions, List<Node> structures) {
         this.value = value;
         this.scope = scope;
+        this.functions = functions;
+        this.structures = structures;
+    }
+
+    @Override
+    public Stream<Node> streamFunctions() {
+        return functions.stream();
+    }
+
+    @Override
+    public Stream<Node> streamStructures() {
+        return structures.stream();
+    }
+
+    @Override
+    public State appendFunctions(Node function) {
+        List<Node> newFunctions = new ArrayList<>(functions);
+        newFunctions.add(function);
+        return new InlineState(value, scope, newFunctions, structures);
+    }
+
+    @Override
+    public State appendStructure(Node structure) {
+        List<Node> newStructures = new ArrayList<>(structures);
+        newStructures.add(structure);
+        return new InlineState(value, scope, functions, newStructures);
     }
 
     @Override
--- src/main/java/com/meti/compile/render/process/State.java
+++ src/main/java/com/meti/compile/render/process/State.java
@@ -2,7 +2,17 @@ package com.meti.compile.render.process;
 
 import com.meti.compile.render.node.Node;
 
+import java.util.stream.Stream;
+
 public interface State extends Node.Container<State> {
+    Stream<Node> streamFunctions();
+
+    Stream<Node> streamStructures();
+
+    State appendFunctions(Node function);
+
+    State appendStructure(Node structure);
+
     boolean has(Node.Group group);
 
     Stack scope();
