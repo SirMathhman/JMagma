b7f7c9a93818ffcf5d6c9ff5ecd5c60f70787f37
SirMathhman <sirmathhman@gmail.com>
2020-10-11 23:08:15 -0400

    Remove semicolon in bad syntax

--- src/test/java/com/meti/compile/render/block/function/ThisFeatureTest.java
+++ src/test/java/com/meti/compile/render/block/function/ThisFeatureTest.java
@@ -13,7 +13,7 @@ public class ThisFeatureTest extends FeatureTest {
                     return this;
                 }
                 """, """
-                def Dummy() => this;
+                def Dummy() => this
                 """);
     }
 }
