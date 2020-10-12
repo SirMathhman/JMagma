efc4ef0e6dff213d1923aaf1258504f4d475bb8c
SirMathhman <sirmathhman@gmail.com>
2020-10-12 18:18:35 -0400

    Complete test

--- src/test/java/com/meti/compile/render/block/structure/ThisResolverTest.java
+++ src/test/java/com/meti/compile/render/block/structure/ThisResolverTest.java
@@ -1,8 +1,11 @@
 package com.meti.compile.render.block.structure;
 
+import com.meti.compile.render.field.InlineField;
+import com.meti.compile.render.process.Stack;
 import org.junit.jupiter.api.Test;
 
 import static com.meti.compile.render.block.structure.ObjectType.ObjectType;
+import static com.meti.compile.render.field.InlineField.Field;
 import static com.meti.compile.render.process.InlineState.State;
 import static com.meti.compile.render.process.MappedStack.Stack_;
 import static com.meti.compile.render.scope.Variable.this_;
@@ -12,7 +15,8 @@ class ThisResolverTest {
 
     @Test
     void resolve() {
-        var state = State(this_, Stack_);
+        var stack_ = Stack_.define(Field("test", null));
+        var state = State(this_, stack_);
         var type = new ThisResolver(state)
                 .resolve()
                 .orElseThrow();
