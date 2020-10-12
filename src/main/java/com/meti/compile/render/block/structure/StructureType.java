9f0dacec04b14c6c914f49330b93b39265d12cd3
SirMathhman <sirmathhman@gmail.com>
2020-10-11 23:28:44 -0400

    Introduce StructureType

---
+++ src/main/java/com/meti/compile/render/block/structure/StructureType.java
@@ -0,0 +1,24 @@
+package com.meti.compile.render.block.structure;
+
+import com.meti.compile.render.type.Type;
+
+import static com.meti.compile.render.type.Type.Group.Structure;
+
+public abstract class StructureType implements Type {
+    protected final String structureName;
+
+    public StructureType(String structureName) {
+        this.structureName = structureName;
+    }
+
+    @Override
+    public String render(String name) {
+        var format = "struct %s %s";
+        return format.formatted(structureName, name);
+    }
+
+    @Override
+    public boolean is(Group group) {
+        return group == Structure;
+    }
+}
---
+++ src/test/java/com/meti/compile/render/block/structure/NamedStructureTypeTest.java
@@ -0,0 +1,27 @@
+package com.meti.compile.render.block.structure;
+
+import org.junit.jupiter.api.Test;
+
+import static com.meti.compile.render.block.structure.NamedStructureType.StructureType;
+import static com.meti.compile.render.type.Type.Group.Structure;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class NamedStructureTypeTest {
+
+    @Test
+    void render() {
+        var actual = StructureType("Dummy").render("value");
+        assertEquals("struct Dummy value", actual);
+    }
+
+    @Test
+    void is() {
+        assertTrue(StructureType("Dummy").is(Structure));
+    }
+
+    @Test
+    void hasEmptyChildren() {
+        assertTrue(StructureType("Dummy").subFields().findAny().isEmpty());
+    }
+}
