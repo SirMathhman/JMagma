4ff0dcc7ce02d3f7c7b259cd19115e996bf383ba
SirMathhman <sirmathhman@gmail.com>
2020-10-31 20:15:28 -0400

    Fix import

--- src/test/java/com/meti/slice/ImmutableStrategyBufferTest.java
+++ src/test/java/com/meti/slice/ImmutableStrategyBufferTest.java
@@ -1,5 +1,6 @@
 package com.meti.slice;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
@@ -12,54 +13,54 @@ class ImmutableStrategyBufferTest {
 
     @Test
     void testEqualsEmpty() {
-        Assertions.assertEquals(EmptyBuffer, EmptyBuffer);
+        assertEquals(EmptyBuffer, EmptyBuffer);
     }
 
     @Test
     void testEqualsContent(){
         StrategyBuffer expected = new ImmutableStrategyBuffer(Collections.emptyList(), 0,"c");
         StrategyBuffer actual = new ImmutableStrategyBuffer(Collections.emptyList(), 0,"c");
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
     void trim() {
         StrategyBuffer expected = new ImmutableStrategyBuffer(List.of("test"));
         StrategyBuffer actual = new ImmutableStrategyBuffer(List.of("test"));
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
     void isLevel() {
-        Assertions.assertTrue(EmptyBuffer.isLevel());
+        assertTrue(EmptyBuffer.isLevel());
     }
 
     @Test
     void isShallow() {
-        Assertions.assertTrue(EmptyBuffer.ascend().isShallow());
+        assertTrue(EmptyBuffer.ascend().isShallow());
     }
 
     @Test
     void descend() {
-        Assertions.assertTrue(EmptyBuffer.ascend().descend().isLevel());
+        assertTrue(EmptyBuffer.ascend().descend().isLevel());
     }
 
     @Test
     void ascend() {
-        Assertions.assertFalse(EmptyBuffer.ascend().isLevel());
+        assertFalse(EmptyBuffer.ascend().isLevel());
     }
 
     @Test
     void append() {
         StrategyBuffer expected = new ImmutableStrategyBuffer(Collections.emptyList(), 0,"c");
         StrategyBuffer actual = EmptyBuffer.append('c');
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
     void complete() {
         StrategyBuffer expected = new ImmutableStrategyBuffer(Collections.emptyList(), 0, "c").complete();
         StrategyBuffer actual = new ImmutableStrategyBuffer(Collections.singletonList("c"), 0, "");
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 }
