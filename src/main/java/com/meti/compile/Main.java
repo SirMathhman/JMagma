0c63a56d6439950b5e9050a400cc9ce69968c9df
SirMathhman <sirmathhman@gmail.com>
2020-11-09 19:04:27 -0500

    Make IO constants inline

--- src/main/java/com/meti/compile/Main.java
+++ src/main/java/com/meti/compile/Main.java
@@ -13,10 +13,6 @@ import java.util.logging.Logger;
 
 public class Main {
     private static final String SourceFormat = "%s.mg";
-    private static final Path Root = Paths.get(".");
-    private static final Path Target = Root.resolve("target.c");
-    private static final Path SourceDirectory = Root.resolve("source");
-    private static final Path Build = Root.resolve(".build");
     private static final Logger logger = Logger.getAnonymousLogger();
 
     public static void main(String[] args) {
@@ -34,7 +30,7 @@ public class Main {
     }
 
     private static void processExceptionally() throws IOException {
-        String content = Files.readString(Main.Build);
+        String content = Files.readString(Paths.get(".").resolve(".build"));
         if (content.isBlank()) logger.log(Level.SEVERE, "No entry point was found.");
         else processBuildContent(content);
     }
@@ -64,43 +60,43 @@ public class Main {
         String[] packageArray = packageTrim.split("\\.");
         String formatted = SourceFormat.formatted(name);
         return Arrays.stream(packageArray)
-                .reduce(Main.SourceDirectory, Path::resolve, (path, path2) -> path2)
+                .reduce(Paths.get(".").resolve("source"), Path::resolve, (path, path2) -> path2)
                 .resolve(formatted);
     }
 
     private static Path formatEntrySimply(String trimmed) {
         String format = "%s.mg";
         String formatted = format.formatted(trimmed);
-        return SourceDirectory.resolve(formatted);
+        return Paths.get(".").resolve("source").resolve(formatted);
     }
 
     private static void ensureBuildFile() {
-        if (!Files.exists(Build)) {
+        if (!Files.exists(Paths.get(".").resolve(".build"))) {
             String format = "Build file did not exist at '%s' and will be created.";
-            String message = format.formatted(Build);
+            String message = format.formatted(Paths.get(".").resolve(".build"));
             logger.log(Level.WARNING, message);
 
             try {
-                Files.createFile(Build);
+                Files.createFile(Paths.get(".").resolve(".build"));
             } catch (IOException e) {
                 String format0 = "Failed to create build file at '%s'.";
-                String message0 = format0.formatted(Build);
+                String message0 = format0.formatted(Paths.get(".").resolve(".build"));
                 logger.log(Level.SEVERE, message0, e);
             }
         }
     }
 
     private static void ensureSourceDirectory() {
-        if (!Files.exists(SourceDirectory)) {
+        if (!Files.exists(Paths.get(".").resolve("source"))) {
             String format = "Source directory did not exist at '%s' and will be created.";
-            String message = format.formatted(SourceDirectory);
+            String message = format.formatted(Paths.get(".").resolve("source"));
             logger.log(Level.WARNING, message);
 
             try {
-                Files.createDirectory(SourceDirectory);
+                Files.createDirectory(Paths.get(".").resolve("source"));
             } catch (IOException e) {
                 String format0 = "Failed to create source directory at '%s'.";
-                String message0 = format0.formatted(SourceDirectory);
+                String message0 = format0.formatted(Paths.get(".").resolve("source"));
                 logger.log(Level.SEVERE, message0, e);
             }
         }
@@ -109,7 +105,7 @@ public class Main {
     private static String compile(Path mainFile) {
         if (Files.exists(mainFile)) {
             String value = readContent(mainFile);
-            ScriptPath scriptPath = new JavaScriptPath(SourceDirectory);
+            ScriptPath scriptPath = new JavaScriptPath(Paths.get(".").resolve("source"));
             return MagmaCompiler.MagmaCompiler(scriptPath).compile(value);
         } else {
             logger.log(Level.SEVERE, "Entry point at '" + mainFile + "' did not exist.");
@@ -127,16 +123,16 @@ public class Main {
     }
 
     private static void deletePreviousTarget() {
-        if (Files.exists(Target)) {
+        if (Files.exists(Paths.get(".").resolve("target.c"))) {
             String format = "Previous target file will be overriden at '%s'.";
-            String message = format.formatted(Target);
+            String message = format.formatted(Paths.get(".").resolve("target.c"));
             logger.log(Level.WARNING, message);
 
             try {
-                Files.delete(Target);
+                Files.delete(Paths.get(".").resolve("target.c"));
             } catch (IOException e) {
                 String format0 = "Failed to delete target file at '%s'.";
-                String message0 = format0.formatted(Target);
+                String message0 = format0.formatted(Paths.get(".").resolve("target.c"));
                 logger.log(Level.WARNING, message0);
             }
         }
@@ -152,14 +148,14 @@ public class Main {
 
     private static void logTargetWritingFailure(int length) {
         String format = "Failed to write output to '%s' of size %d.";
-        String message = format.formatted(Main.Target, length);
+        String message = format.formatted(Paths.get(".").resolve("target.c"), length);
         logger.log(Level.SEVERE, message);
     }
 
     private static void writeToTargetExceptionally(CharSequence output) throws IOException {
-        Files.writeString(Main.Target, output);
+        Files.writeString(Paths.get(".").resolve("target.c"), output);
         String format = "Wrote %d characters to target at '%s'.";
-        String message = format.formatted(output.length(), Main.Target);
+        String message = format.formatted(output.length(), Paths.get(".").resolve("target.c"));
         logger.log(Level.INFO, message);
     }
 }
