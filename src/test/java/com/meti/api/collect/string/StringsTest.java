215dc34552f5d488e07ccc4b7b374a85dd283cd4
SirMathhman <sirmathhman@gmail.com>
2020-12-06 00:54:04 -0500

    Add StringsTest

---
+++ src/test/java/com/meti/api/collect/string/StringsTest.java
@@ -0,0 +1,36 @@
+package com.meti.api.collect.string;
+
+import com.meti.api.collect.stream.StreamException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringsTest {
+
+	@Test
+	void concat() {
+		assertEquals("xy", Strings.concat("x", "y"));
+	}
+
+	@Test
+	void compareToEquals() {
+		assertEquals(0, Strings.compareTo("x", "x"));
+	}
+
+	@Test
+	void compareToLess(){
+		assertEquals(-3, Strings.compareTo("a", "d"));
+	}
+
+	@Test
+	void compareToGreater(){
+		assertEquals(3, Strings.compareTo("d", "a"));
+	}
+
+	@Test
+	void stream() throws StreamException {
+		assertEquals('t', Strings.stream("test")
+				.head()
+				.orElse('\0'));
+	}
+}
