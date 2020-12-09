cd94fa0c5017ae863039549e4bac56544adaea34
SirMathhman <sirmathhman@gmail.com>
2020-12-08 19:33:24 -0500

    Add more tests

--- src/test/java/com/meti/exec/compile/FeatureTest.java
+++ src/test/java/com/meti/exec/compile/FeatureTest.java
@@ -2,12 +2,35 @@ package com.meti.exec.compile;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class FeatureTest extends Feature{
+class FeatureTest extends Feature {
 
 	@Test
 	void spaces() {
 		assertEquals("1 2 3 4", formatTarget("   1  2  3    4"));
+	}
+
+	@Test
+	void tabs() {
+		assertEquals("", formatTarget("\t"));
+	}
+
+	@Test
+	void newLines() {
+		assertEquals("", formatTarget("\n"));
+	}
+
+	@Test
+	void normal() {
+		assertEquals(formatTarget("""
+				struct Wrapper[T] {
+					const value : T
+				}
+				"""), formatTarget("""
+				struct Wrapper[T] {
+					const value : T
+				}
+				"""));
 	}
 }
