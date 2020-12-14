3f58c35f16d02362a85cc022383398d29d97000d
SirMathhman <sirmathhman@gmail.com>
2020-12-14 14:59:13 -0500

    Replace Comparable with Equatable

--- src/main/java/com/meti/exec/compile/render/field/Field.java
+++ src/main/java/com/meti/exec/compile/render/field/Field.java
@@ -1,9 +1,9 @@
 package com.meti.exec.compile.render.field;
 
-import com.meti.api.core.Comparable;
+import com.meti.api.core.Equatable;
 import com.meti.exec.compile.render.Renderable;
 
-public interface Field<F> extends Renderable, Comparable<F> {
+public interface Field<F> extends Renderable, Equatable<F> {
 	enum Flag {
 		Const
 	}
