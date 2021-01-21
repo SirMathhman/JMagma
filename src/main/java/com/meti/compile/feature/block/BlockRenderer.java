54c06a2a693154f8936b123b26e906ac94393ad7
SirMathhman <sirmathhman@gmail.com>
2021-01-21 11:56:56 -0500

    Add BlockRenderer

---
+++ src/main/java/com/meti/compile/feature/block/BlockRenderer.java
@@ -0,0 +1,26 @@
+package com.meti.compile.feature.block;
+
+import com.meti.compile.stage.Renderer;
+import com.meti.compile.token.*;
+
+import java.util.Optional;
+
+public class BlockRenderer implements Renderer<Token> {
+	public static final Renderer<Token> BlockRenderer_ = new BlockRenderer();
+
+	public BlockRenderer() {
+	}
+
+	@Override
+	public Optional<Token> render(Token token) {
+		if (Tokens.is(token, GroupAttribute.Block)) {
+			var lines = token.apply(Token.Query.Lines).asTokenList();
+			var lineAccumulator = new Parent(lines);
+			var complete = Parents.format("{%t}")
+					.format(lineAccumulator)
+					.complete();
+			return Optional.of(complete);
+		}
+		return Optional.empty();
+	}
+}
--- src/main/java/com/meti/compile/token/Parent.java
+++ src/main/java/com/meti/compile/token/Parent.java
@@ -9,8 +9,25 @@ public record Parent(List<Token> lines) implements Token {
 		return switch (query) {
 			case Group -> GroupAttribute.Parent;
 			case Lines -> new TokenListAttribute(lines);
+			case Value -> new StringAttribute(computeValue());
 			default -> throw new UnsupportedOperationException();
 		};
+	}
+
+	private String computeValue() {
+		var builder = new StringBuilder();
+		for (Token line : lines) {
+			var isContent = Tokens.is(line, GroupAttribute.Content);
+			var isParent = Tokens.is(line, GroupAttribute.Parent);
+			if (isContent || isParent) {
+				builder.append(line.apply(Query.Value));
+			} else {
+				var format = "Cannot render lines of type '%s'.";
+				var message = format.formatted(line.apply(Query.Group));
+				throw new UnsupportedOperationException(message);
+			}
+		}
+		return builder.toString();
 	}
 
 	@Override
