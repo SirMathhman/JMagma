2c496d29e634334a2977eee92cd71dac7200aeab
SirMathhman <sirmathhman@gmail.com>
2021-01-20 20:58:45 -0500

    Add test for NIOFIle.writeAsString

---
+++ src/test/java/com/meti/api/java/io/NIOFileTest.java
@@ -0,0 +1,26 @@
+package com.meti.api.java.io;
+
+import com.meti.api.magma.io.IOException_;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static com.meti.api.java.io.NIOFileSystem.NIOFileSystem_;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NIOFileTest {
+
+	@Test
+	void writeAsString() throws IOException_, IOException {
+		NIOFileSystem_.Root()
+				.ensureAsDirectory()
+				.resolve("Main.mg")
+				.ensureAsFile()
+				.writeAsString("test");
+		var nativePath = Paths.get(".", "Main.mg");
+		assertEquals("test", Files.readString(nativePath));
+		Files.delete(nativePath);
+	}
+}
