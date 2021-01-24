c369928e4c65fb0f6e0705dd9652aa76b0e035bb
SirMathhman <sirmathhman@gmail.com>
2021-01-24 16:27:06 -0500

    ???

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -1,7 +1,6 @@
 package com.meti;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -14,40 +13,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ApplicationTest {
-	private static final Path TargetDirectory = Paths.get(".", "test1");
-	private static final Path Target = Paths.get(".", "test1", "Main.c");
-	private static final Path Build = Paths.get(".", "build");
-	private static final Path SourceDirectory = Paths.get(".", "test0");
-	private static final Path SourceFile = Paths.get(".", "test0", "Main.mg");
-
-	@BeforeEach
-	void setUp() throws IOException {
-		if (!Files.exists(Build)) Files.createFile(Build);
-		if (!Files.exists(SourceDirectory)) Files.createDirectories(SourceDirectory);
-		if (!Files.exists(SourceFile)) Files.createFile(SourceFile);
-
-		Files.writeString(Build, "test0\ntest1");
-		Files.writeString(SourceFile, "def main() : I16 => {return 0;}");
-	}
+	private static Path targetDirectory;
+	private static Path target;
+	private static Path build;
+	private static Path sourceDirectory;
+	private static Path sourceFile;
 
 	@AfterEach
 	void tearDown() throws IOException {
-		Files.deleteIfExists(Target);
-		Files.deleteIfExists(TargetDirectory);
-		Files.deleteIfExists(SourceFile);
-		Files.deleteIfExists(SourceDirectory);
-		Files.deleteIfExists(Build);
+		Files.deleteIfExists(target);
+		Files.deleteIfExists(targetDirectory);
+		Files.deleteIfExists(sourceFile);
+		Files.deleteIfExists(sourceDirectory);
+		Files.deleteIfExists(build);
 	}
 
 	@Test
 	void target_exists() throws IOException {
+		setUp("test0", "Main.mg", "test1", "Main.c", "test0\ntest1\n", "def main() : I16 => {return 0;}");
 		Application_.run();
-		assertTrue(Files.exists(Target));
+		assertTrue(Files.exists(target));
+	}
+
+	void setUp(String sourceDirectory, String sourceName, String targetDirectory, String targetName, String buildContent, String sourceContent) throws IOException {
+		var root = Paths.get(".");
+		build = root.resolve("build");
+
+		ApplicationTest.sourceDirectory = root.resolve(sourceDirectory);
+		sourceFile = ApplicationTest.sourceDirectory.resolve(sourceName);
+
+		ApplicationTest.targetDirectory = root.resolve(targetDirectory);
+		target = ApplicationTest.targetDirectory.resolve(targetName);
+
+		if (!Files.exists(build)) Files.createFile(build);
+		if (!Files.exists(ApplicationTest.sourceDirectory)) Files.createDirectories(ApplicationTest.sourceDirectory);
+		if (!Files.exists(sourceFile)) Files.createFile(sourceFile);
+
+		Files.writeString(build, buildContent);
+		Files.writeString(sourceFile, sourceContent);
 	}
 
 	@Test
 	void target_content() throws IOException {
+		setUp("test0", "Main.mg", "test1", "Main.c", "test0\ntest1\n", "def main() : I16 => {return 0;}");
 		Application_.run();
-		assertEquals("int main(){return 0;}", Files.readString(Target));
+		assertEquals("int main(){return 0;}", Files.readString(target));
 	}
 }
