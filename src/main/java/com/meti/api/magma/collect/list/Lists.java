c980fe09867824ade78ebe136f9e5563248c463e
SirMathhman <sirmathhman@gmail.com>
2021-02-03 14:15:01 -0500

    Add ArrayLists

--- src/main/java/com/meti/api/magma/collect/list/Lists.java
+++ src/main/java/com/meti/api/magma/collect/list/Lists.java
@@ -11,17 +11,42 @@ public class Lists {
 	public Lists() {
 	}
 
-	public static <T extends Equatable<T>> boolean contains(List<T> list, T element) {
-		return contains(list, element, Equatable::equalsTo);
+	public static <T extends Equatable<T>> boolean equalsTo(List<T> self, List<T> other) {
+		return equalsTo(self, other, Equatable::equalsTo);
 	}
 
-	public static <T> boolean contains(List<T> list, T element, F2<T, T, Boolean> equator) {
-		return findFirst(list, element, equator).isPresent();
+	public static <T> boolean equalsTo(List<T> self, List<T> other, F2<T, T, Boolean> equator) {
+		var selfSize = self.size();
+		var otherSize = other.size();
+		if (selfSize == otherSize) {
+			for (int i = 0; i < selfSize; i++) {
+				try {
+					var selfElement = self.apply(i);
+					var otherElement = self.apply(i);
+					if (!equator.apply(selfElement, otherElement)) {
+						return false;
+					}
+				} catch (IndexException e) {
+					return false;
+				}
+			}
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	private static <T> Option<Integer> findFirst(List<T> list, T element, F2<T, T, Boolean> equator) {
+	public static <T extends Equatable<T>> boolean contains(List<T> self, T element) {
+		return contains(self, element, Equatable::equalsTo);
+	}
+
+	public static <T> boolean contains(List<T> self, T element, F2<T, T, Boolean> equator) {
+		return findFirst(self, element, equator).isPresent();
+	}
+
+	private static <T> Option<Integer> findFirst(List<T> self, T element, F2<T, T, Boolean> equator) {
 		try {
-			for (int i = 0; i < list.size(); i++) if (equator.apply(list.apply(i), element)) return new Some<>(i);
+			for (int i = 0; i < self.size(); i++) if (equator.apply(self.apply(i), element)) return new Some<>(i);
 			return new None<>();
 		} catch (IndexException e) {
 			return new None<>();
