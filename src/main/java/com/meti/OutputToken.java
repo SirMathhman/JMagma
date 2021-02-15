318a926e543bb77ca8a79f24c268533b068b1f77
SirMathhman <sirmathhman@gmail.com>
2021-02-15 18:36:47 -0500

    Remove unused code

--- src/main/java/com/meti/OutputToken.java
+++
@@ -1,8 +0,0 @@
-package com.meti;
-
-public record OutputToken(Output value) implements Token {
-	@Override
-	public Attribute apply(Attribute.Name name) {
-		return new OutputAttribute(value);
-	}
-}
