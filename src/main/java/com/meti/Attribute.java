c6250886d7894206474a1028c51f8b57bcdf6fca
SirMathhman <sirmathhman@gmail.com>
2021-02-15 18:18:41 -0500

    Add Attribute.computeInput

--- src/main/java/com/meti/Attribute.java
+++ src/main/java/com/meti/Attribute.java
@@ -3,6 +3,10 @@ package com.meti;
 public interface Attribute {
 	Output asOutput();
 
+	default Input computeInput() throws AttributeException {
+		throw new AttributeException("Not input.");
+	}
+
 	enum Name {
 		Output
 	}
