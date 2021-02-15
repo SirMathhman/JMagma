3deea1833c132a8f1418e22f21c64954bc4f0cde
SirMathhman <sirmathhman@gmail.com>
2021-02-15 17:39:49 -0500

    Add test for integers

--- src/main/java/com/meti/Compiler.java
+++ src/main/java/com/meti/Compiler.java
@@ -8,6 +8,17 @@ public class Compiler {
 
 	String compile(String input) throws CompileException {
 		if (input.isBlank()) throw new CompileException("Input is blank.");
-		return "int main(){return 0;}";
+		else if (isInteger(input)) {
+			return input;
+		} else return "int main(){return 0;}";
+	}
+
+	private boolean isInteger(String input) {
+		for (int i = 0; i < input.length(); i++) {
+			if (!Character.isDigit(input.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
