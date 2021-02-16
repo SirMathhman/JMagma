5d3de6b848f74aedd46f4a09e45d9a7af415d5f7
SirMathhman <sirmathhman@gmail.com>
2021-02-16 14:54:13 -0500

    Tabs?

--- src/main/java/com/meti/Attribute.scala
+++ src/main/java/com/meti/Attribute.scala
@@ -2,24 +2,24 @@ package com.meti
 
 object Attribute {
 
-  sealed trait Name extends Attribute
+	sealed trait Name extends Attribute
 
-  object Name {
+	object Name {
 
-    case object Group extends Name
+		case object Group extends Name
 
-    case object Content extends Name
+		case object Content extends Name
 
-    case object Value extends Name
+		case object Value extends Name
 
-  }
+	}
 
 }
 
 trait Attribute {
-  @throws[AttributeException]
-  def computeInput: Input = throw AttributeException("Not input.")
+	@throws[AttributeException]
+	def computeInput: Input = throw AttributeException("Not input.")
 
-  @throws[AttributeException]
-  def computeToken: Token = throw AttributeException("Not a token.")
+	@throws[AttributeException]
+	def computeToken: Token = throw AttributeException("Not a token.")
 }
--- src/main/java/com/meti/CollectionException.scala
+++ src/main/java/com/meti/CollectionException.scala
@@ -1,7 +1,7 @@
 package com.meti
 
 object CollectionException {
-  def apply(message: String = null, cause: Throwable = null): CollectionException = new CollectionException(message, cause)
+	def apply(message: String = null, cause: Throwable = null): CollectionException = new CollectionException(message, cause)
 }
 
 class CollectionException(message: String, cause: Throwable) extends Exception(message, cause) {
