f6d53b196f804d039b9a19eb79f993cea9325954
SirMathhman <sirmathhman@gmail.com>
2021-03-14 14:02:31 -0400

    Add implementation of ApplicationTest.test

--- src/test/com/meti/ApplicationTest.java
+++ src/test/com/meti/ApplicationTest.java
@@ -2,9 +2,18 @@ package com.meti;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ApplicationTest {
 	@Test
-	void test() {
-
+	void test() throws IOException {
+		var path = Paths.get(".", "Main.c");
+		Files.writeString(path, "int main(){return 0;}");
+		assertEquals("int main(){return 0;}", Files.readString(path));
+		Files.deleteIfExists(path);
 	}
 }
